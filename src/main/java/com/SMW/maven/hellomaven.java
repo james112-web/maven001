1caa72a2b2571ddedcc3eb5942833c95199386a5
smw <songjames@aliyun.com>
2019-09-03 09:29:31 +0800

    my  eclipse  第一次

---
+++ src/main/java/com/SMW/maven/hellomaven.java
@@ -0,0 +1,7 @@
+package com.SMW.maven;
+
+public class hellomaven {
+	public String  name() {
+		return  "你好啊  java";
+	}
+}
---
+++ src/test/java/com/SMW/maven/hellomavenTest.java
@@ -0,0 +1,9 @@
+package com.SMW.maven;
+import org.junit.Test;
+public class hellomavenTest {
+	@Test
+	public void test() {
+        hellomaven  mv = new hellomaven();
+        System.out.println(mv.name());
+	}
+}
